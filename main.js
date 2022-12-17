36077ff962e0b8ffa49f945e2508357dc82f88c7
Hamidreza maharlu <maharlu.hamidreza@gmail.com>
2022-12-17 12:05:50 +0100

    make payment section

--- main.js
+++ main.js
@@ -75,17 +75,17 @@ function renderFood(allFoods) {
                     </p>
                     <div class="foods__detail--wrapper--counter">
                         <div class="foods__detail--decAndIncBtn">
-                            <div class="btn--add" onclick="countPlus(this)">
+                            <div class="btn--add" data-fee=${food.price} data-id=${food.id} onclick="countAdd(this)">
                                 <i class="bi bi-plus-square-fill"></i>
                             </div>
-                            <span class="num" id=${food.id}>0</span>
-                            <div class="btn--delete" onclick="countMinus(this)">
+                            <span class="num" id=count${food.id}>0</span>
+                            <div class="btn--delete" data-fee=${food.price} data-id=${food.id} onclick="countDelete(this)">
                                 <i class="bi bi-dash-square-fill"></i>
                             </div>
                         </div>
                         <div>
                             <p class="foods__detail--Price">
-                                <span id=${food.id}>0</span> تومان
+                                <span id=price${food.id}>0</span> تومان
                             </p>
                         </div>
                     </div>
@@ -94,4 +94,60 @@ function renderFood(allFoods) {
         foods.insertAdjacentHTML('beforeend', html);
     })
 }
+
 renderFood(foods)
+
+function countAdd(el) {
+    let count = el.parentElement.children[1].textContent;
+    let price = +document.getElementById(`price${el.dataset.id}`).textContent;
+    let allPrice = +document.getElementById("all-price").textContent;
+    document.getElementById("all-price").textContent = allPrice + +el.dataset.fee
+    tax(+document.getElementById("all-price").textContent)
+    total()
+    price = +price + +el.dataset.fee;
+    count++
+    el.parentElement.children[1].textContent = count;
+    document.getElementById(`price${el.dataset.id}`).textContent = price;
+}
+
+function countDelete(el) {
+    let count = el.parentElement.children[1].textContent;
+    console.log(count)
+    let price = document.getElementById(`price${el.dataset.id}`).textContent
+    let allPrice = +document.getElementById("all-price").textContent;
+    if (allPrice > 0 && count> 0) {
+        document.getElementById("all-price").textContent = allPrice - +el.dataset.fee;
+        tax(+document.getElementById("all-price").textContent)
+        total()
+    }
+    if (count > 0 || price > 0) {
+        price = +price - +el.dataset.fee;
+        count--;
+    } else {
+        count = 0
+        price = 0
+    }
+    el.parentElement.children[1].textContent = count;
+    document.getElementById(`price${el.dataset.id}`).textContent = price;
+}
+
+function tax(price) {
+    document.getElementById("service").textContent=price*5/100
+}
+function total() {
+    const toPay = +document.getElementById("service").textContent + +document.getElementById("all-price").textContent
+    document.getElementById("pay").textContent=toPay
+}
+
+function openModal() {
+    document.getElementById("modal").classList.remove("close")
+    document.getElementById("modal").classList.add("show");
+    document.querySelector(".modalBg").style.display = "flex"
+
+}
+function closeModal() {
+    document.getElementById("modal").classList.remove("show")
+    document.getElementById("modal").classList.add("close")
+    document.querySelector(".modalBg").style.display = "none";
+    window.location.reload()
+}
